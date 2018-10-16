f2050f454b490f376eb36a98bad9c43c4d9b5519
Fikret Ceylan <ofcyln@gmail.com>
2018-10-16 11:08:23 +0300

    successPageComponent integration with UI/UX implementation

--- src/app/job/success/success.component.ts
+++ src/app/job/success/success.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'app-success',
     templateUrl: './success.component.html',
     styleUrls: ['./success.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class SuccessComponent implements OnInit {
     constructor() {}
